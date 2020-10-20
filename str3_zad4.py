2ff4f242da8892d3a2da5687db11809a81e6711c
andjelao <73198069+andjelao@users.noreply.github.com>
2020-10-21 00:20:56 +0200

    Update str3_zad4.py

--- str3_zad4.py
+++ str3_zad4.py
@@ -6,10 +6,14 @@ a= int(input("a: "))
 b= int(input("b: "))
 c= int(input("c: "))
 
-try:   #domen za sqrt je od nula do beskonacno
-    x1= (-b+ math.sqrt(b**2-4*a*c )) / 2*a
-    x2= (-b- math.sqrt(b**2-4*a*c )) / 2*a
-except:  # izuzetak ako je korijen negativan- kompleksan broj
-    x1= (-b+ cmath.sqrt(b**2-4*a*c )) / 2*a
-    x2= (-b- cmath.sqrt(b**2-4*a*c )) / 2*a
-print(x1 , x2)
+def kvadratna_jednacina(a,b,c):
+    try:   #domen za sqrt je od nula do beskonacno
+        x1= (-b+ math.sqrt(b**2-4*a*c )) / 2*a
+        x2= (-b- math.sqrt(b**2-4*a*c )) / 2*a
+    except:  # izuzetak ako je korijen negativan- kompleksan broj
+        x1= (-b+ cmath.sqrt(b**2-4*a*c )) / 2*a
+        x2= (-b- cmath.sqrt(b**2-4*a*c )) / 2*a
+    finally:
+        print(x1 , x2)
+
+kvadratna_jednacina(a,b,c)
