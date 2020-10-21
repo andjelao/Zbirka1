cee6319609a12d8a950b9ac7ab29e98bd46db338
andjelao <73198069+andjelao@users.noreply.github.com>
2020-10-21 22:16:42 +0200

    Update str1_zad4.py

--- str1_zad4.py
+++ str1_zad4.py
@@ -1,42 +1,37 @@
 # unosenje datuma u obliku dd.mm.gg, stampa datum sa imenom mjeseca umjesto broja
-datum= input(" Unesite datum u formatu dan.mjesec.godina.: ")
+datum = input(" Unesite datum u formatu dd.mm.gg. : ")
 
-prva_tacka=datum.index(".")
-zadnja_tacka=datum.rfind(".")
-druga_tacka= datum.find(".", prva_tacka+1, zadnja_tacka)
+def formatiraj_datum(datum):
+    datum = datum.split(".")
+    dan = datum[0]
+    mjesec = datum[1]
+    godina = datum[2]
 
-mjesec= datum[prva_tacka+1 : druga_tacka]
-dan= datum[0 : prva_tacka+1]
-godina= datum[druga_tacka+1: zadnja_tacka+1]
+    if mjesec == "01":
+        mjesec = "januar"
+    elif mjesec == "02":
+        mjesec = "februar"
+    elif mjesec == "03":
+        mjesec = "mart"
+    elif mjesec == "04":
+        mjesec = "april"
+    elif mjesec == "05":
+        mjesec= "maj"
+    elif mjesec == "06":
+        mjesec = "jun"
+    elif mjesec =="07":
+        mjesec = "jul"
+    elif mjesec =="08":
+        mjesec = "avgust"
+    elif mjesec == "09":
+        mjesec = "septembar"
+    elif mjesec =="10":
+        mjesec = "oktobar"
+    elif mjesec == "11":
+        mjesec = "novembar"
+    else: 
+        mjesec = "decembar"
 
-if mjesec=="1":
-    mjesec= "januar"
-elif mjesec=="2":
-    mjesec="februar"
-elif mjesec== "3":
-    mjesec= "mart"
-elif mjesec== "4":
-    mjesec= "april"
-elif mjesec== "5":
-    mjesec= "maj"
-elif mjesec=="6":
-    mjesec="jun"
-elif mjesec=="7":
-    mjesec="jul"
-elif mjesec=="8":
-    mjesec="avgust"
-elif mjesec=="9":
-    mjesec="septembar"
-elif mjesec=="10":
-    mjesec="oktobar"
-elif mjesec=="11":
-    mjesec= "novembar"
-else: 
-    mjesec= "decembar"
+    print(dan + ". " + mjesec + " " + godina + ".")
 
-print(dan, mjesec, godina)
-
-
-
-    
-
+formatiraj_datum(datum)
