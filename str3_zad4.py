93856a9b3a8ed8ac4e06985709471e893349f8ac
andjelao <73198069+andjelao@users.noreply.github.com>
2020-10-21 22:17:32 +0200

    Update str3_zad4.py

--- str3_zad4.py
+++ str3_zad4.py
@@ -2,18 +2,23 @@
 import math
 import cmath
 print("Unesite koeficijente kvadratne jednacine: ")
-a= int(input("a: "))
-b= int(input("b: "))
-c= int(input("c: "))
+a = int(input("a: "))
+b = int(input("b: "))
+c = int(input("c: "))
 
-def kvadratna_jednacina(a,b,c):
-    try:   #domen za sqrt je od nula do beskonacno
-        x1= (-b+ math.sqrt(b**2-4*a*c )) / 2*a
-        x2= (-b- math.sqrt(b**2-4*a*c )) / 2*a
-    except:  # izuzetak ako je korijen negativan- kompleksan broj
-        x1= (-b+ cmath.sqrt(b**2-4*a*c )) / 2*a
-        x2= (-b- cmath.sqrt(b**2-4*a*c )) / 2*a
-    finally:
-        print(x1 , x2)
-
+def kvadratna_jednacina(a, b, c):
+    d= b**2 - 4*a*c
+    if d > 0:
+        x1 = (-b + math.sqrt(b**2 - 4*a*c )) / 2*a
+        x2 = (-b - math.sqrt(b **2 - 4*a*c )) / 2*a
+        print(x1)
+        print(x2)
+    elif d == 0:
+        x= -b / 2 * a
+        print(x)
+    else:
+        x1 = (-b + cmath.sqrt(b**2 - 4*a*c )) / 2*a
+        x2 = (-b - cmath.sqrt(b**2 - 4*a*c )) / 2*a
+        print(x1)
+        print(x2)
 kvadratna_jednacina(a,b,c)
